d7dca114e86743bfc7cd8282cadb5dab17588588
Ferdinand Ade <ade@apiomat.com>
2020-12-05 19:33:00 +0100

    test for calculating same denominator

--- src/main/kotlin/Fraction.kt
+++ src/main/kotlin/Fraction.kt
@@ -1,10 +1,28 @@
-data class Fraction(private val number: Int,private val denominator: Int) {
+import kotlin.math.max
+import kotlin.math.min
 
-    fun add(fraction: Fraction): Fraction {
-        return Fraction(this.number + fraction.number, calculateDenominator(fraction))
+data class Fraction(val number: Int, val denominator: Int) {
+
+    fun add(otherFraction: Fraction): Fraction {
+        val commonDenominator = calculateDenominator(otherFraction)
+        return Fraction(this.number + otherFraction.number, commonDenominator)
     }
 
-    private fun calculateDenominator(fraction: Fraction): Int {
-        return if(this.denominator == fraction.denominator) this.denominator else this.denominator
+    private fun calculateDenominator(otherFraction: Fraction): Int {
+        return if(this.denominator == otherFraction.denominator) {
+            this.denominator
+        } else {
+            findCommonDenominator(otherFraction)
+        }
     }
+
+    private fun findCommonDenominator(otherFraction: Fraction): Int {
+        return if(isEven(this.denominator) && isEven(otherFraction.denominator)) {
+            min(this.denominator, otherFraction.denominator)
+        } else {
+            1
+        }
+    }
+
+    private fun isEven(denominator1: Int) = denominator1 % 2 == 0
 }
--- src/test/kotlin/FractionTest.kt
+++ src/test/kotlin/FractionTest.kt
@@ -3,9 +3,16 @@ import kotlin.test.assertEquals
 
 class FractionTest {
     @Test
-    fun `should add two fractions`() {
+    fun `should add two fractions with same denominator`() {
         val result = Fraction(1,2).add(Fraction(1,2))
 
         assertEquals(Fraction(2,2), result)
+    }
+
+    @Test
+    fun `should calculate common denominator`() {
+        val result = Fraction(1,2).add(Fraction(1,4))
+
+        assertEquals(2, result.denominator)
     }
 }
